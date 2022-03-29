cc59290a53a9900ff06714aa330bb3bbda246897
Karthik Bhat B <karb1@publicisgroupe.net>
2022-03-29 12:48:12 +0530

    Testing parameter

--- src/main/java/com/cr/automation/App.java
+++ src/main/java/com/cr/automation/App.java
@@ -3,15 +3,41 @@ package com.cr.automation;
 import org.apache.log4j.BasicConfigurator;
 
 public class App {
-	 public static void main(String args[]) {
-		 BasicConfigurator.configure();  
-		 DocxGenerator dg = new DocxGenerator();
-		 
-		 try {
-			dg.test();
+	public static void main(String args[]) {
+		// BasicConfigurator.configure();
+		DocxGenerator dg = new DocxGenerator();
+		System.out.println("brand " + args[0]);
+		System.out.println("locale " + args[1]);
+		System.out.println("country " + args[2]);
+		System.out.println("region " + args[3]);
+		System.out.println("testUrl " + args[4]);
+		System.out.println("uatUrl " + args[5]);
+		System.out.println("uatPreviewUrl " + args[6]);
+		System.out.println("prodUrl " + args[7]);
+		System.out.println("prodPreviewUrl " + args[8]);
+		System.out.println("isDualLanguage " + args[9]);
+		System.out.println("secondLocale " + args[10]);
+
+		String brand = args[0];
+		String locale = args[1];
+		String country = args[2];
+		String region = args[3];
+		String testUrl = args[4];
+		String uatUrl = args[5];
+		String uatPreviewUrl = args[6];
+		String prodUrl = args[7];
+		String prodPreviewUrl = args[8];
+
+		String isDualLanguage = args[9];
+		String secondLocale = args[10];
+
+		try {
+			// dg.test();
+			dg.test2(brand, locale, country, region, testUrl, uatUrl, uatPreviewUrl, prodUrl, prodPreviewUrl,
+					isDualLanguage, secondLocale);
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-	 }
+	}
 }
--- src/main/java/com/cr/automation/DocxGenerator.java
+++ src/main/java/com/cr/automation/DocxGenerator.java
@@ -30,10 +30,10 @@ public class DocxGenerator {
 		String uatPreviewUrl = "uat-origin123-preview.jeep.com.mx";
 		String prodUrl = "prod.jeep.com.mx";
 		String prodPreviewUrl = "prod-preview.jeep.com.mx";
-		
+
 		String isDualLanguage = "false";
 		String secondLocale = "en_mx";
-		
+
 		/* Setting variables for template */
 		variables.put("brandDescription", brand.toUpperCase() + " " + country.toUpperCase());
 		variables.put("folderName", brand + "-" + country);
@@ -62,7 +62,7 @@ public class DocxGenerator {
 			templateInputStream2 = this.getClass().getClassLoader().getResourceAsStream("templateTest.docx");
 			templateInputStream3 = this.getClass().getClassLoader().getResourceAsStream("templateProd.docx");
 		}
-		
+
 		/* UAT and UAT preview Document generation */
 		WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(templateInputStream);
 		MainDocumentPart documentPart = wordMLPackage.getMainDocumentPart();
@@ -106,6 +106,92 @@ public class DocxGenerator {
 		os3.close();
 		outputStream3.close();
 		templateInputStream3.close();
+
+	}
+
+	public void test2(String brand, String locale, String country, String region, String testUrl, String uatUrl,
+			String uatPreviewUrl, String prodUrl, String prodPreviewUrl, String isDualLanguage, String secondLocale)
+			throws Exception {
+		
+		HashMap<String, String> variables = new HashMap<>();
+		InputStream templateInputStream;
+		InputStream templateInputStream2;
+		InputStream templateInputStream3;
+
+	
+
+		/* Setting variables for template */
+		variables.put("brandDescription", brand.toUpperCase() + " " + country.toUpperCase());
+		variables.put("folderName", brand + "-" + country);
+		variables.put("uatUrl", uatUrl);
+		variables.put("uatPreviewUrl", uatPreviewUrl);
+		variables.put("prodUrl", prodUrl);
+		variables.put("prodPreviewUrl", prodPreviewUrl);
+		variables.put("testUrl", testUrl);
+		variables.put("brandLocale", brand.substring(0, 1).toUpperCase() + brand.substring(1) + " "
+				+ country.substring(0, 1).toUpperCase() + country.substring(1));
+		variables.put("brand", brand);
+		variables.put("country", country);
+		variables.put("locale", locale);
+		variables.put("locale", secondLocale);
+		variables.put("region", region);
+
+		String directoryName = "target/";
+
+		/* Reading Templates */
+		if (isDualLanguage.equalsIgnoreCase("true")) {
+			templateInputStream = this.getClass().getClassLoader().getResourceAsStream("templateUat2.docx");
+			templateInputStream2 = this.getClass().getClassLoader().getResourceAsStream("templateTest2.docx");
+			templateInputStream3 = this.getClass().getClassLoader().getResourceAsStream("templateProd2.docx");
+		} else {
+			templateInputStream = this.getClass().getClassLoader().getResourceAsStream("templateUat.docx");
+			templateInputStream2 = this.getClass().getClassLoader().getResourceAsStream("templateTest.docx");
+			templateInputStream3 = this.getClass().getClassLoader().getResourceAsStream("templateProd.docx");
+		}
+
+		/* UAT and UAT preview Document generation */
+		WordprocessingMLPackage wordMLPackage = WordprocessingMLPackage.load(templateInputStream);
+		MainDocumentPart documentPart = wordMLPackage.getMainDocumentPart();
+		VariablePrepare.prepare(wordMLPackage);
+		documentPart.variableReplace(variables);
+		OutputStream os = new FileOutputStream(
+				new File(directoryName + variables.get("brandDescription") + "_Domain Setup_UAT AND UAT Preview.docx"));
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+		wordMLPackage.save(outputStream);
+		outputStream.writeTo(os);
+		os.close();
+		outputStream.close();
+		templateInputStream.close();
+
+		/* Test Document generation */
+
+		WordprocessingMLPackage wordMLPackage2 = WordprocessingMLPackage.load(templateInputStream2);
+		MainDocumentPart documentPart2 = wordMLPackage2.getMainDocumentPart();
+		VariablePrepare.prepare(wordMLPackage2);
+		documentPart2.variableReplace(variables);
+		OutputStream os2 = new FileOutputStream(
+				new File(directoryName + variables.get("brandDescription") + "_Domain Setup_Test.docx"));
+		ByteArrayOutputStream outputStream2 = new ByteArrayOutputStream();
+		wordMLPackage2.save(outputStream2);
+		outputStream2.writeTo(os2);
+		os2.close();
+		outputStream2.close();
+		templateInputStream2.close();
+
+		/* PROD and PROD preview Document generation */
+
+		WordprocessingMLPackage wordMLPackage3 = WordprocessingMLPackage.load(templateInputStream3);
+		MainDocumentPart documentPart3 = wordMLPackage3.getMainDocumentPart();
+		VariablePrepare.prepare(wordMLPackage3);
+		documentPart3.variableReplace(variables);
+		OutputStream os3 = new FileOutputStream(
+				new File(directoryName + variables.get("brandDescription") + "_Domain Setup_Prod.docx"));
+		ByteArrayOutputStream outputStream3 = new ByteArrayOutputStream();
+		wordMLPackage3.save(outputStream3);
+		outputStream3.writeTo(os3);
+		os3.close();
+		outputStream3.close();
+		templateInputStream3.close();	
 
 	}
 }
